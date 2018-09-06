8ecbfd3c8ba6803caadc1a6c0e481221dfb9092a
DESKTOP-CVQ6K9K\PC-LISBON <carotenuto.attilio@gmail.com>
2018-09-06 21:54:05 +0100

    Adding first UI localisation example

---
+++ Assets/unity-i18n-tools/Examples/SimpleUIExample/Scripts/TextFieldTest.cs
@@ -0,0 +1,38 @@
+﻿namespace SimpleUIExample
+{
+	using UnityEngine;
+
+	public class TextFieldTest : MonoBehaviour 
+	{
+
+		private int languageIndex = 0;
+
+		public void onNextButtonPressed()
+		{
+			if(languageIndex >= LocalisationManager.instance.getLanguagesAmount()-1)
+			{
+				languageIndex = 0;
+			}
+			else
+			{
+				languageIndex++;
+			}
+
+			LocalisationManager.instance.setLanguage(languageIndex);
+		}
+
+		public void onPreviousButtonPressed()
+		{
+			if(languageIndex <= 0)
+			{
+				languageIndex = LocalisationManager.instance.getLanguagesAmount()-1;
+			}
+			else
+			{
+				languageIndex--;
+			}
+
+			LocalisationManager.instance.setLanguage(languageIndex);
+		}
+	}
+}
