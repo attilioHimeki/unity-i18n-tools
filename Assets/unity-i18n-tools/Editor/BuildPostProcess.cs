d9cc66479720c77189e1aeff783521d9b8cdc6c9
Attilio Carotenuto <carotenuto.attilio@gmail.com>
2019-03-27 01:04:32 +0100

    Post-process script to set supported languages on Xcode builds

---
+++ Assets/unity-i18n-tools/Editor/BuildPostProcess.cs
@@ -0,0 +1,48 @@
+﻿using UnityEditor;
+using UnityEditor.Callbacks;
+
+#if UNITY_IOS
+using UnityEditor.iOS.Xcode;
+using System.IO;
+#endif
+
+public static class BuildPostProcess
+{
+    /// <summary>
+    /// Add languages that you intend to support on iTunes Connect.
+    /// </summary>
+	private static readonly string[] supportedLanguages = 
+    {
+        LanguageIsoCode.EN
+        // Replace with your supported languages here
+    };
+
+
+    [PostProcessBuildAttribute]
+    static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject)
+    {
+        if (target == BuildTarget.iOS)
+        {
+#if UNITY_IOS
+            var pListPath = Path.Combine(pathToBuiltProject, "Info.plist");
+            var pList = new PlistDocument();
+            pList.ReadFromFile(pListPath);
+
+			var root = pList.root;
+
+            // Adding supported languages to XCode project
+            if(supportedLanguages.Length > 0)
+            {
+                PlistElementArray languages = root.CreateArray("CFBundleLocalizations");
+                foreach(var l in supportedLanguages) 
+                {
+                    languages.AddString(l);
+                }
+            }
+
+            pList.WriteToFile(pListPath);
+#endif
+        }
+    }
+
+}
